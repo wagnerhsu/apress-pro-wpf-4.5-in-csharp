189ad09110b406d8042480f5f96348e08353510c
Wagner Xu <wagnerhsu@hotmail.com>
2020-08-25 22:27:51 +0800

    update files

--- Pro WPF 4.5 in C#/Chapter31/Multithreading/Window1.xaml.cs
+++ Pro WPF 4.5 in C#/Chapter31/Multithreading/Window1.xaml.cs
@@ -1,23 +1,12 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using System.Threading;
+using System.Windows;
 using System.Windows.Threading;
-
 
 namespace Multithreading
 {
     public partial class Window1 : System.Windows.Window
     {
-
         public Window1()
         {
             InitializeComponent();
@@ -40,16 +29,15 @@ namespace Multithreading
             thread.Start();
         }
 
-
         private void UpdateTextRight()
         {
             Thread.Sleep(TimeSpan.FromSeconds(5));
 
             this.Dispatcher.BeginInvoke(DispatcherPriority.Normal,
-                (ThreadStart) delegate()
-                {
-                    txt.Text = "Here is some new text.";
-                }
+                (ThreadStart)delegate ()
+               {
+                   txt.Text = "Here is some new text.";
+               }
                 );
         }
 
